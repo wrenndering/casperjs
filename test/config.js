5dc030cca23ab80ef0817450b3e2f8aeadd7517c
jprice <jprice@yodle.com>
2016-06-13 10:48:45 -0400

    add functionality for logging all tests to log.xml

--- test/config.js
+++ test/config.js
@@ -1,7 +1,8 @@
 var config = {
     "urls": [
         "http://casperjs.org/",
-        "http://phantomjs.org/"
+        "http://phantomjs.org/",
+        "https://nodejs.org/en/"
     ],
     "name": "Title exists",
     "numberOfTests": 2,
@@ -9,7 +10,16 @@ var config = {
     "tests":{
         "test1": function (test, url) {
             casper.echo("Page Title: " + casper.getTitle());
-            return test["assertExists"]("title", "title element is present for: " + url + " - page title: " + casper.getTitle());
+            return casper.then(function () {
+                test["assertExists"]("title", "title element is present for: " + url + " - page title: " + casper.getTitle());
+            });
+        },
+        "test2": function (test, url) {
+            return casper.then(function () {
+                test["assertExists"]("#foo", "foo id is present for: " + url);
+                // this test will fail, but shows the functionality of having casper.then() wrap the test
+                // normally Casper will stop after the first test fail, but casper.then() lets all the tests run and log to log.xml 
+            });
         }
     }
 };
